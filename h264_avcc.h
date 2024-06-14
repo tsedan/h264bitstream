4f745b3a249df3af593419d951ecc5cc4533371a
Tomer Sedan <tsedan@proton.me>
2024-06-14 03:17:41 +0000

    fix fpic issue

--- h264_avcc.h
+++ h264_avcc.h
@@ -1,8 +1,8 @@
 #ifndef _H264_AVCC_H
-#define _H264_AVCC_H        1
+#define _H264_AVCC_H 1
 
-#include <stdint.h>
 #include <assert.h>
+#include <stdint.h>
 
 #include "bs.h"
 #include "h264_stream.h"
@@ -13,11 +13,10 @@ extern "C" {
 
 /**
    AVC decoder configuration record, ISO/IEC 14496-15:2004(E), Section 5.2.4.1
-   Seen in seen in mp4 files as 'avcC' atom 
+   Seen in seen in mp4 files as 'avcC' atom
    Seen in flv files as AVCVIDEOPACKET with AVCPacketType == 0
 */
-typedef struct
-{
+typedef struct {
   int configurationVersion; // = 1
   int AVCProfileIndication;
   int profile_compatibility;
@@ -26,16 +25,16 @@ typedef struct
   int lengthSizeMinusOne;
   // bit(3) reserved = '111'b;
   int numOfSequenceParameterSets;
-  sps_t** sps_table;
+  sps_t **sps_table;
   int numOfPictureParameterSets;
-  pps_t** pps_table;
+  pps_t **pps_table;
 } avcc_t;
 
-avcc_t* avcc_new();
-void avcc_free(avcc_t* avcc);
-int read_avcc(avcc_t* avcc, h264_stream_t* h, bs_t* b);
-int write_avcc(avcc_t* avcc, h264_stream_t* h, bs_t* b);
-void debug_avcc(avcc_t* avcc);
+avcc_t *avcc_new();
+void avcc_free(avcc_t *avcc);
+int read_avcc(avcc_t *avcc, h264_stream_t *h, bs_t *b);
+int write_avcc(avcc_t *avcc, h264_stream_t *h, bs_t *b);
+void debug_avcc(avcc_t *avcc);
 
 #ifdef __cplusplus
 }
